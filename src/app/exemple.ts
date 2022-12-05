c1d32f7dc1e9732a1dfc23876d35a8cbe6ce9200
dymafr <32801166+dymafr@users.noreply.github.com>
2022-12-05 13:45:18 +0100

    1 file modified

--- src/app/exemple.ts
+++ src/app/exemple.ts
@@ -1,17 +1,17 @@
-import { HttpClient } from "@angular/common/http";
-import { Component } from "@angular/core";
-import { MatTableDataSource } from "@angular/material/table";
-import { Observable, of } from "rxjs";
-import { catchError, map } from "rxjs/operators";
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Component({
-  selector: "app-exemple-component",
-  templateUrl: "exemple.html",
-  styleUrls: ["exemple.scss"]
+  selector: 'app-exemple-component',
+  templateUrl: 'exemple.html',
+  styleUrls: ['exemple.scss'],
 })
 export class ExempleComponent {
-  displayedColumns = ["created", "state", "number", "title"];
-  exampleDatabase: ExampleHttpDao | null;
+  displayedColumns = ['created', 'state', 'number', 'title'];
+  exampleDatabase!: ExampleHttpDao;
   dataSource = new MatTableDataSource();
 
   resultsLength = 0;
@@ -21,9 +21,9 @@ export class ExempleComponent {
 
   ngAfterViewInit() {
     this.exampleDatabase = new ExampleHttpDao(this.http);
-    this.exampleDatabase!.getRepoIssues("true", "ascending", 1)
+    this.exampleDatabase!.getRepoIssues('true', 'ascending', 1)
       .pipe(
-        map(data => {
+        map((data) => {
           this.isLoading = false;
           this.resultsLength = data.total_count;
           return data.items.slice(0, 5);
@@ -33,7 +33,7 @@ export class ExempleComponent {
           return of([]);
         })
       )
-      .subscribe(data => (this.dataSource.data = data));
+      .subscribe((data) => (this.dataSource.data = data));
   }
 }
 
@@ -57,9 +57,10 @@ export class ExampleHttpDao {
     order: string,
     page: number
   ): Observable<GithubApi> {
-    const href = "https://api.github.com/search/issues";
-    const requestUrl = `${href}?q=repo:angular/components&sort=${sort}&order=${order}&page=${page +
-      1}`;
+    const href = 'https://api.github.com/search/issues';
+    const requestUrl = `${href}?q=repo:angular/components&sort=${sort}&order=${order}&page=${
+      page + 1
+    }`;
 
     return this._httpClient.get<GithubApi>(requestUrl);
   }
